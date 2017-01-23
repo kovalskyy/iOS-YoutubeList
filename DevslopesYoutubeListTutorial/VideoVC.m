810a8780e908273da2bf06660186f74b9ad244fe
Kacper Kowalski <kowal@Mac-mini-Kacper.local>
2017-01-23 22:13:06 +0100

    small adjustments

--- DevslopesYoutubeListTutorial/VideoVC.m
+++ DevslopesYoutubeListTutorial/VideoVC.m
@@ -17,6 +17,7 @@
 @end
 
 @implementation VideoVC
+
 - (IBAction)DoneBtnTapped:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
@@ -38,9 +39,9 @@
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
     
-    NSString *css = @".container {postion: relative; width: 100%; height: 0; padding-bottom: 56.25%;} .video { postion: absolute; top: 0; left: 0; width: 100%; height: 100%;}";
+    NSString *css = @".container {position: relative; width: 100%; height: 0; padding-bottom: 56.25%; } .video { position: absolute; top: 0; left: 0; width: 100%; height: 100%;}";
     
-       NSString* js = [NSString stringWithFormat:
+    NSString* js = [NSString stringWithFormat:
                     @"var styleNode = document.createElement('style');\n"
                     "styleNode.type = \"text/css\";\n"
                     "var styleText = document.createTextNode('%@');\n"
@@ -48,6 +49,7 @@
                     "document.getElementsByTagName('head')[0].appendChild(styleNode);\n",css];
     NSLog(@"js:\n%@",js);
     [self.webView stringByEvaluatingJavaScriptFromString:js];
+
 }
 
 
